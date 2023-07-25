c1674ef65032cc8b94889db4a0cf50033ead5691
Luca Carrozzo <carrozzo.lc@gmail.com>
2023-07-25 08:37:14 +0200

    section 7 updated

--- understanding-typescript/section_07/generics/src/app.ts
+++ understanding-typescript/section_07/generics/src/app.ts
@@ -34,8 +34,30 @@ function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
 
 console.log(countAndDescribe(['Sports', 'Cooking']));
 
-function extractAndConvert(obj: object, key: string) {
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
   return obj[key];
 }
 
-console.log(extractAndConvert({}, 'name'));
+console.log(extractAndConvert({name: 'Max'}, 'name'));
+
+class DataStorage<T> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Max');
+textStorage.addItem('manu');
+textStorage.removeItem('Max');
+console.log(textStorage.getItems());
